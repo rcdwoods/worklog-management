49799bfb410dfb2916fdfa13ea1421942c351062
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-01-20 08:40:31 -0300

    :hammer: Add endpoint to retrieve employees paginated

--- src/main/kotlin/com/richards/worklogmanagement/resource/EmployeesResource.kt
+++ src/main/kotlin/com/richards/worklogmanagement/resource/EmployeesResource.kt
@@ -1,8 +1,12 @@
 package com.richards.worklogmanagement.resource
 
+import com.richards.worklogmanagement.converter.EmployeeConverter
 import com.richards.worklogmanagement.service.EmployeeService
 import io.swagger.api.EmployeesApi
 import io.swagger.model.Employee
+import io.swagger.model.EmployeesPaginated
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EmployeesResource(
-    private val employeeService: EmployeeService
+    private val employeeService: EmployeeService,
+    private val employeeConverter: EmployeeConverter
 ) : EmployeesApi {
 
-    @GetMapping
-    override fun retrieveEmployees(
-        @RequestParam page: Long,
-        @RequestParam size: Long
-    ): ResponseEntity<List<Employee>> =
-        employeeService.retrieveEmployees()
+    override fun retrieveEmployees(): ResponseEntity<EmployeesPaginated> {
+        val pageRequest = PageRequest.of(0, 0)
+        val employeesFound = employeeService.retrieveEmployees(pageRequest)
+        val employeesPaginated = EmployeesPaginated().apply {
+            this.content = employeeConverter.convert(employeesFound.content)
+            this.page = pageRequest.pageNumber
+            this.perPage = pageRequest.pageSize
+        }
+
+        return ResponseEntity.ok(employeesPaginated)
+    }
 
 }
