8f1029bfb237c11f9ad509655e1a040e27ff9e01
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-01-20 09:42:27 -0300

    :hammer: Add endpoints to register and update employees

--- src/main/kotlin/com/richards/worklogmanagement/resource/EmployeesResource.kt
+++ src/main/kotlin/com/richards/worklogmanagement/resource/EmployeesResource.kt
@@ -9,25 +9,46 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/v1/employees")
 class EmployeesResource(
     private val employeeService: EmployeeService,
     private val employeeConverter: EmployeeConverter
 ) : EmployeesApi {
 
-    override fun retrieveEmployees(): ResponseEntity<EmployeesPaginated> {
-        val pageRequest = PageRequest.of(0, 0)
-        val employeesFound = employeeService.retrieveEmployees(pageRequest)
+    @GetMapping
+    override fun retrieveEmployees(
+        @RequestParam("page") page: Int,
+        @RequestParam("per_page", defaultValue = "5") perPage: Int
+    ): ResponseEntity<EmployeesPaginated> {
+        val employeesFound = employeeService.retrieveEmployees(PageRequest.of(page, perPage))
         val employeesPaginated = EmployeesPaginated().apply {
-            this.content = employeeConverter.convert(employeesFound.content)
-            this.page = pageRequest.pageNumber
-            this.perPage = pageRequest.pageSize
+            this.content = employeeConverter.toEmployeesDTO(employeesFound.content)
+            this.page = page
+            this.perPage = perPage
         }
 
         return ResponseEntity.ok(employeesPaginated)
     }
 
+    @PostMapping
+    override fun registerEmployee(@RequestBody employee: Employee): ResponseEntity<Employee> {
+        val employeeConverted = employeeConverter.toEmployee(employee)
+        val savedEmployee = employeeService.saveEmployee(employeeConverted)
+        return ResponseEntity.ok(employeeConverter.toEmployeeDTO(savedEmployee))
+    }
+
+    @PutMapping
+    override fun updateEmployee(@RequestBody employee: Employee): ResponseEntity<Employee> {
+        val employeeConverted = employeeConverter.toEmployee(employee)
+        val updatedEmployee = employeeService.updateEmployee(employeeConverted)
+        return ResponseEntity.ok(employeeConverter.toEmployeeDTO(updatedEmployee))
+    }
 }
