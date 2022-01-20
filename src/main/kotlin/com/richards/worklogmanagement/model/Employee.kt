8aeb4a7905501ceacb38a6f17230e204d9c90fb8
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-01-20 08:37:19 -0300

    :hammer: Add equals and hashCode to Employee

--- src/main/kotlin/com/richards/worklogmanagement/model/Employee.kt
+++ src/main/kotlin/com/richards/worklogmanagement/model/Employee.kt
@@ -12,4 +12,24 @@ data class Employee (
     val id: Long? = null,
     val name: String? = null,
     val email: String? = null
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Employee
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (email != other.email) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (email?.hashCode() ?: 0)
+        return result
+    }
+}
